a9ff0adf937a913599e2dcf08983084033e77893
balbo97 <balbosauro1@gmail.com>
2024-01-30 13:29:54 +1000

    milestone-1 completed

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+    // Milestone 1
+    // Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+
+    function generate_password($password_length){
+        $password = '';
+        $characters = 'abcdefghijklmnopqrstuvwxyz'.strtoupper('abcdefghijklmnopqrstuvwxyz').'0123456789!£$%&/()=?[]@#{}';
+
+        $characters_length = strlen($characters);
+
+        while(strlen($password) < $password_length){
+            $index = rand(0, $characters_length - 1);
+
+            $char = $characters[$index];
+
+            $password = $password.$char;
+        }
+
+        return $password;
+    }
+
+    if(isset($_GET['password-length']) && $_GET['password-length'] != ''){
+        $password_length = $_GET['password-length'];
+
+        $password = generate_password($password_length);
+        
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +40,10 @@
     <div class="container">
         <div class="row">
            <div class="col-12">
+                <h2>
+
+                    <?php echo $password ?>
+                </h2>
 
                 <!-- Milestone 1
                 Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. -->
@@ -20,7 +52,7 @@
                         <div class="col-12">
 
                             
-                            <input type="number" name="password-lenght" placeholder="Password lenght" class="form-control">
+                            <input type="number" name="password-length" placeholder="Password length" class="form-control">
                         </div>
                         <div class="col-12 my-3">
                             <button type="submit" class="btn btn-primary">Send</button>
